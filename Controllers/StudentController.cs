0f619cf8891b377dbbb4721c3d3e34795c09bf94
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-10-09 15:31:55 +0700

    menambahkan model

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using contohaspcore.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace contohaspcore.Controllers
@@ -12,6 +13,34 @@ namespace contohaspcore.Controllers
             /*List<string> lstNama = new List<string>{"erick","budi","bambang"};
             return new JsonResult(lstNama);*/
             return View();
+        }
+
+        public IActionResult GetStudents(){
+            Student student1 = new Student {
+                Nim = "112233",
+                Nama = "Erick",
+                Alamat = "Lempongsari Raya",
+                Telp = "08156778894"
+            };
+            Student student2 =new Student{
+                Nim = "112244",
+                Nama = "Budi",
+                Alamat = "Gowongan Lor",
+                Telp = "0815677999"
+            };
+            Student student3 = new Student{
+                Nim = "112255",
+                Nama = "Bambang",
+                Alamat = "Jl Kaliurang km 6",
+                Telp = "08156774433"
+            };
+
+            List<Student> lstStudent = new List<Student>();
+            lstStudent.Add(student1);
+            lstStudent.Add(student2);
+            lstStudent.Add(student3);
+
+            return View(lstStudent);
         }
 
         [HttpPost]
---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+namespace contohaspcore.Models
+{
+    public class Student
+    {
+        public string Nim { get; set; }
+        public string Nama { get; set; }    
+        public string Alamat { get; set; }
+        public string Telp { get; set; }
+    }
+}
